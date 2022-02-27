035e8027fcdfde54356a33192ae15a1624e3d68b
alifsyahputra <alif.syahputra@nexsoft.co.id>
2022-02-27 20:12:49 +0700

    membuat modul baru

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alif-github/learn-go-modules
+
+go 1.16
---
+++ say_hello.go
@@ -0,0 +1,5 @@
+package learn_go_modules
+
+func SayHelloWorld(say string) string {
+	return "Hello World, Hello " + say
+}
