13a398cfe69806d1a9d98cbe08e283740e25daf9
alifsyahputra <alif.syahputra@nexsoft.co.id>
2022-02-27 21:09:18 +0700

    major changer v1 to v2

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/alif-github/learn-go-modules
+module github.com/alif-github/learn-go-modules/v2
 
 go 1.16
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package learn_go_modules
 
-func SayHelloWorld(say string) string {
-	return "Hello Jakarta, Hello " + say
+func SayHelloWorld(say1 string, say2 string) string {
+	return "Hello "+ say1 +", Hello " + say2
 }
